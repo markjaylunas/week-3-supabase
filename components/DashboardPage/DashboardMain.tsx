da31435966e05f67e22e24b0b88bb820fe477a71
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-10-31 19:28:51 +0800

    Add dashboard page

--- components/DashboardPage/DashboardMain.tsx
+++ components/DashboardPage/DashboardMain.tsx
@@ -1,5 +1,145 @@
-const DashboardMain = () => {
-  return <div>DashboardMain</div>;
+import {
+  Avatar,
+  Button,
+  Container,
+  Grid,
+  Space,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import { ChangeEvent, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { Profile } from "../../pages/p/dashboard";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { toast } from "react-toastify";
+
+const DashboardMain = ({ profile }: { profile: Profile | null }) => {
+  const [userProfile, setUserProfile] = useState(profile);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const supabaseClient = useSupabaseClient();
+
+  const handleSignOut = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("/api/sign-out");
+      if (response.data) {
+        router.replace("/sign-in");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  const handleCompressedUpload = async (e: ChangeEvent<HTMLInputElement>) => {
+    const toastLoading = toast.loading("Please wait...");
+    const currentAvatarPath =
+      userProfile && userProfile.avatar
+        ? getAvatarPath(userProfile.avatar)
+        : "";
+
+    const inputFile = e.target.files;
+    if (inputFile === null) return;
+    const image = inputFile[0];
+    const fileReader = new FileReader();
+    try {
+      fileReader.readAsDataURL(image || new Blob());
+      fileReader.onload = async () => {
+        if (image) {
+          const { data, error } = await supabaseClient.functions.invoke(
+            "compress-image",
+            {
+              body: image,
+            }
+          );
+          if (error) throw error;
+          if (data) {
+            await removeAvatar(currentAvatarPath);
+            const publicURL = await getAvatarUrl(data.data.path);
+            await updateAvatarURL(publicURL);
+            toast.update(toastLoading, {
+              render: "Uploaded",
+              type: "success",
+              isLoading: false,
+              autoClose: 5000,
+            });
+            router.reload();
+          }
+        }
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getAvatarPath = (avatar: string) => {
+    if (avatar.length > 0) {
+      return avatar.split("avatars/")[1].split("?")[0];
+    } else {
+      return avatar;
+    }
+  };
+
+  const removeAvatar = async (path: string) => {
+    const { data: removeData, error: removeError } =
+      await supabaseClient.storage.from("avatars").remove([path]);
+    if (removeError) throw removeError;
+    return removeData;
+  };
+  const getAvatarUrl = async (path: string) => {
+    const {
+      data: { publicUrl },
+    } = await supabaseClient.storage.from("avatars").getPublicUrl(path);
+    const formattedUrl = `${publicUrl}?t=${new Date().toISOString()}`;
+    return formattedUrl;
+  };
+
+  const updateAvatarURL = async (avatarURL: string) => {
+    const { data: updateData, error: updateError } = await supabaseClient
+      .from("User")
+      .update({ avatar: avatarURL })
+      .eq("id", userProfile?.id)
+      .select("*");
+    if (updateError) throw updateError;
+    else {
+      setUserProfile(updateData[0] as unknown as Profile);
+    }
+  };
+  return (
+    <Container>
+      <Stack>
+        <Grid align={"space-between"}>
+          <Grid.Col>
+            <Title>Dashboard</Title>
+          </Grid.Col>
+          <Grid.Col>
+            <Button disabled={loading ? true : false} onClick={handleSignOut}>
+              Sign out
+            </Button>
+          </Grid.Col>
+        </Grid>
+        <Stack>
+          <Avatar
+            src={profile?.avatar}
+            alt={profile?.email}
+            radius="lg"
+            size="xl"
+          />
+
+          <Text size="lg">{profile?.email}</Text>
+          <Space h="lg" />
+          <input
+            type="file"
+            accept="image/jpeg"
+            onChange={handleCompressedUpload}
+          />
+        </Stack>
+      </Stack>
+    </Container>
+  );
 };
 
 export default DashboardMain;
--- pages/p/dashboard.tsx
+++ pages/p/dashboard.tsx
@@ -1,17 +1,34 @@
-import { useUser } from "@supabase/auth-helpers-react";
+import { Loader } from "@mantine/core";
+import { withPageAuth } from "@supabase/auth-helpers-nextjs";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import axios from "axios";
 import { NextPage } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import DashboardMain from "../../components/DashboardPage/DashboardMain";
 import Layout from "../../components/Layout";
 
+export type Profile = {
+  id: string;
+  created_at: string;
+  email: string;
+  avatar: string;
+  user_id: string;
+};
+
 const Dashboard: NextPage = () => {
-  const user = useUser();
-  const router = useRouter();
+  const [profile, setProfile] = useState<Profile | null>(null);
+  const supabase = useSupabaseClient();
+
   useEffect(() => {
-    if (user === null) router.replace("/sign-in");
-  }, [router, user]);
+    const fetchProfile = async () => {
+      const { data } = await supabase.auth.getSession();
+      const user_id = data.session?.user.id;
+      const response = await axios.post("/api/profile", { user_id });
+      setProfile(response.data.data[0]);
+    };
+    fetchProfile();
+  }, [supabase.auth]);
 
   return (
     <>
@@ -19,10 +36,14 @@ const Dashboard: NextPage = () => {
         <title>Dashboard</title>
       </Head>
       <Layout>
-        <DashboardMain />
+        {profile !== null ? <DashboardMain profile={profile} /> : <Loader />}
       </Layout>
     </>
   );
 };
 
 export default Dashboard;
+
+export const getServerSideProps = withPageAuth({
+  redirectTo: "/sign-in",
+});

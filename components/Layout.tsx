eb159d7fcf47fea1592f2fbb62bd9e86c58c303a
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-10-31 11:45:25 +0800

    Add layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
+import Link from "next/link";
+
+export default function AppShellDemo({ children }: { children: JSX.Element }) {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  return (
+    <AppShell
+      styles={{
+        main: {
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      navbar={
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
+          <Link href="/register">
+            <Text>Register</Text>
+          </Link>
+          <Link href="/sign-in">
+            <Text>Sign In</Text>
+          </Link>
+        </Navbar>
+      }
+      header={
+        <Header height={70} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>Week 3</Text>
+          </div>
+        </Header>
+      }
+    >
+      {children}
+    </AppShell>
+  );
+}

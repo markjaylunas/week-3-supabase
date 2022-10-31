f05bdd707fdedced9fe1b2e30fc614517b57a911
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-10-31 15:53:06 +0800

    Added page protection

--- pages/p/dashboard.tsx
+++ pages/p/dashboard.tsx
@@ -1,9 +1,18 @@
+import { useUser } from "@supabase/auth-helpers-react";
 import { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import DashboardMain from "../../components/DashboardPage/DashboardMain";
 import Layout from "../../components/Layout";
 
 const Dashboard: NextPage = () => {
+  const user = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    if (user === null) router.replace("/sign-in");
+  }, [router, user]);
+
   return (
     <>
       <Head>

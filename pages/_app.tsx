031a92dfb3a415357418288d627b5e7741e3977a
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-10-31 11:26:25 +0800

    Add mantine and session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { MantineProvider } from "@mantine/core";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+type AppSession = {
+  session: Session;
+};
+
+const App = ({ Component, pageProps }: AppProps<AppSession>) => {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.session}
+    >
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: "dark" }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </SessionContextProvider>
+  );
+};
+
+export default App;

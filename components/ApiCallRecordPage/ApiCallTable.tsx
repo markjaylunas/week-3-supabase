bda681b20909d5e6876361d615fffad692e61bd6
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-11-02 13:26:56 +0800

    remove concat in api_name

--- components/ApiCallRecordPage/ApiCallTable.tsx
+++ components/ApiCallRecordPage/ApiCallTable.tsx
@@ -5,12 +5,11 @@ import ApiCallList from "../../types/apiCall.types";
 const ApiCallTable = ({ apiCallList }: { apiCallList: ApiCallList | null }) => {
   if (apiCallList === null) return <Loader />;
   const rows = apiCallList.map((api) => {
-    const api_name = "https://www.test.com/api/" + api.api_name;
     const date = new Date(api.created_at || "");
     const dateFormatted = `${date.getMonth()}/${date.getDay()}/${date.getFullYear()}`;
     return (
       <tr key={api.id}>
-        <td>{api_name}</td>
+        <td>{api.api_name}</td>
         <td>{dateFormatted}</td>
         <td>{api.called_by}</td>
       </tr>
